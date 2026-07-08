4c299e5ac268db2ac7fe1591ec6d179d3c8d10a3
dalonso <masdavidalonso@gmail.com>
2026-07-08 20:43:34 +0200

    chore: configure eslint, prettier, gitignore and run initial formatting pass

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules']),
+
+  {
+    files: ['**/*.{js,jsx}'],
+
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      }
+    },
+
+    plugins: {
+      'react-hooks': reactHooks
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+    }
+  },
+
+  eslintConfigPrettier
+])
